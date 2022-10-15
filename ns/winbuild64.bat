b0cb050f3ed30e2a5f43cf7b55646ace25a3dab3
ek <kalininei@yandex.ru>
2022-10-15 18:52:13 +0300

    build script and readme adopted for different vs versions

--- ns/winbuild64.bat
+++ ns/winbuild64.bat
@@ -4,9 +4,6 @@ REM =============== System options
 REM Define generator
 SET CMGenerator="Visual Studio 16 2019"
 
-REM Toolset
-SET CMToolset=v141
-
 REM Architechture: x64 or Win32
 SET CMArch=x64
 
@@ -16,7 +13,7 @@ SET CMBOOST_ROOT="c:/libs/boost_1_78_0"
 REM root amgc library
 SET CMAMGCL_ROOT="c:/libs/amgcl-1.4.2"
 
-cmake -G %CMGenerator% -T %CMToolset% -A %CMArch% ..^
+cmake -G %CMGenerator% -A %CMArch% ..^
 	-DBOOST_ROOT=%CMBOOST_ROOT% ^
 	-DAMGCL_ROOT=%CMAMGCL_ROOT%
 
