0159a5d2d74c89a7e5e307d9874f5d13ac1ae913
Damir Safin <damirkasafin@engee.cloud>
2023-01-09 17:57:11 +0300

    SeidelSolver

---
+++ ns/lib/slae/seidel_solver.hpp
@@ -0,0 +1,25 @@
+#ifndef SEIDEL_SOLVER_HPP
+#define SEIDEL_SOLVER_HPP
+
+#include "common.hpp"
+#include "slae/csrmat.hpp"
+#include "slae/a_matrix_solver.hpp"
+
+class SeidelSolver: public AMatrixSolver{
+public:
+	SeidelSolver(double eps=0.001, int max_iterations=1000);
+	~SeidelSolver(){};
+
+	void set_matrix(const CsrStencil& mat, const std::vector<double>& mat_values) override;
+
+	void solve(const std::vector<double>& rhs, std::vector<double>& ret) const override;
+private:
+	CsrStencil stencil;
+	std::vector<double> val;
+	double eps;
+	int max_iterations;
+
+	double solve_residual(const std::vector<double>& x, const std::vector<double>& rhs) const;
+};
+
+#endif
---
+++ ns/progs/tests/test_seidel_solver.cpp
@@ -0,0 +1,69 @@
+#include "slae/seidel_solver.hpp"
+#include "slae/csrmat.hpp"
+#include <string>
+
+double vec_vec_residual(const std::vector<double>& v1, const std::vector<double>& v2){
+	double s = 0.0;
+	for (int i=0;i<v1.size(); i++){
+		s += (v1[i] - v2[i]) * (v1[i] - v2[i]);
+	}
+	return sqrt(s);
+}
+
+bool test_1(double eps){
+    /* {{5.0, 0.0, 0.0}, {1.0, 2.0, 0.0}, {0.0, 0.0, 6.0}}.{1.0, 2.0, 3.0} == {5.0, 5.0, 18.0}
+        addres = [0, 1, 3, 4]
+        columns = [1, 1, 2, 3]
+        values = [5.0, 1.0, 2.0, 6.0]*/
+    std::vector<std::set<int>> stencil_set{{0}, {0, 1}, {2}};
+    std::vector<double> vals{5.0, 2.0, 1.0, 6.0}; 
+    std::vector<double> rhs{5.0, 5.0, 18.0}; 
+
+    std::vector<double> answer{1.0, 2.0, 3.0}; 
+    std::vector<double> result; 
+    
+    CsrStencil st = CsrStencil::build(stencil_set);
+    SeidelSolver solver(0.0001, 1000);
+    solver.set_matrix(st, vals);
+    solver.solve(rhs, result);
+    double err = vec_vec_residual(result, answer);
+    
+    return err < eps;
+}
+
+bool test_2(double eps){
+    /* {{3.0, 2.3, 0.0, 0.0}, {0.0, 3.5, 0.0, 0.0}, {0.0, 0.0, 8.0, 7.0}, {-1.0, 0.0, 3.0, 15.0}}.{2., 0., 9., -1.} = {6., 0., 65., 10.}
+        addres = [0, 2, 3, 7, 8]
+        values = [3.0, 2.3, 3.5, 8.0, 7.0, -1.0, 3.0, 15.0]
+        columns = [1, 2, 2, 3, 4, 1, 3, 4]*/
+    std::vector<std::set<int>> stencil_set{{0, 1}, {1}, {2, 3}, {0, 2, 3}};
+    std::vector<double> vals{3.0, 2.3, 3.5, 8.0, 7.0,15.0, -1.0, 3.0}; 
+    std::vector<double> rhs{6.0, 0.0, 65.0, 10.0}; 
+
+    std::vector<double> answer{2.0, 0.0, 9.0, -1.0}; 
+    std::vector<double> result;  
+    
+    CsrStencil st = CsrStencil::build(stencil_set);
+    SeidelSolver solver(0.0001, 1000);
+    solver.set_matrix(st, vals);
+    solver.solve(rhs, result);
+    double err = vec_vec_residual(result, answer);
+    
+    return err < eps;
+}
+
+
+void print_test_result(bool result, std::string test_name){
+    if (result)
+        std::cout << "Test \"" << test_name << "\" completed\n";
+    else
+        std::cout << "Test \"" << test_name << "\" failed\n";
+}
+
+
+int main(){
+    print_test_result(test_1(0.001), "Seidel Solver: test_1");
+    print_test_result(test_2(0.001), "Seidel Solver: test_2");
+
+    return 0;
+}
